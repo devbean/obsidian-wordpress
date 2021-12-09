197fe1550e6ff78b27f3807f3a7b322296d377bc
Cheng Liang <devbean@outlook.com>
2021-12-09 15:32:59 +0800

    Remove comments.

--- README.md
+++ README.md
@@ -4,17 +4,18 @@ This plugin makes you publish Obsidian documents to WordPress.
 
 ## How to use
 
-First set your WordPress URL, user name and password in settings.
+Set your WordPress URL in settings as well as username if you want.
 
-Put cursor in MarkDown editor use **Publish to WordPress** command in
-[Command Palette](https://help.obsidian.md/Plugins/Command+palette).
-The document will be published to WordPress that you set.
+Put cursor in a MarkDown editor, then use **Publish to WordPress** in
+[Command Palette](https://help.obsidian.md/Plugins/Command+palette)
+or you could show a button in side in settings.
+The document will be published to the WordPress URL that you set.
 
 ## Limits
 
 The plugin uses XML-RPC to publish to WordPress. XML-RPC is enabled
-by default of WordPress but some security experts suggest to disable it.
-Some shared hosts might disable XML-RPC by default which you have
+by default but some may disable it because of security problems.
+And some shared hosts might disable XML-RPC by default which you have
 no way to enable it. So this won't work if XML-RPC is disabled.
 
-This will be changed in the future by adding RESTful API support.
+This will be changed in future versions by adding WordPress RESTful API support.
--- src/wp-publish-view.ts
+++ src/wp-publish-view.ts
@@ -31,18 +31,7 @@ export class WordPressPublishView extends ItemView {
   }
 
   private draw(): void {
-    // const container = this.containerEl.children[1];
-    // const rootEl = document.createElement('div');
-
-    // const button = rootEl.createDiv({ cls: 'action-button', title: 'Publish' });
-    // button.onClickEvent(() => {
-    //   console.log('Do publish');
-    // });
-    // const text = rootEl.createSpan('Publish');
-    // button.appendChild(text);
-    // // button.appendChild(icons['wp-logo']);
     const actionButtonsControlDiv = this.contentEl.createEl('div');
-    // vantageButtonsControlDiv.addClass("setting-item-control");
     new ButtonComponent(actionButtonsControlDiv)
       .setButtonText('Publish')
       .setClass('mod-cta')
@@ -50,17 +39,6 @@ export class WordPressPublishView extends ItemView {
         const client = createWordPressClient(this.app, this.plugin, 'xmlrpc');
         client.newPost().then();
       });
-
-    // const button = container.createEl('div', { cls: 'book' });
-    // button.createEl('div', { text: 'Publish', cls: 'action-button' });
-    // button.onClickEvent(() => {
-    //   console.log(this.settings);
-    //
-    // });
-    // book.createEl('small', { text: 'Sönke Ahrens', cls: 'book__author' });
-
-    // containerEl.empty();
-    // containerEl.appendChild(rootEl);
   }
 
 }
