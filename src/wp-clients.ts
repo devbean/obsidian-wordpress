b6584e73892ab6a52915ab00b9a00cab2c5752fd
Cheng Liang <devbean@outlook.com>
2022-12-12 10:38:31 +0800

    fix: Remove client cache.

--- src/wp-clients.ts
+++ src/wp-clients.ts
@@ -5,9 +5,6 @@ import { WpXmlRpcClient } from './wp-xml-rpc-client';
 import { WpRestClient, WpRestClientMiniOrangeContext } from './wp-rest-client';
 import { WordPressClient } from './wp-client';
 
-
-const wpClientsCache = new Map<ApiType, WordPressClient>();
-
 export function getWordPressClient(
   app: App,
   plugin: WordpressPlugin
@@ -16,25 +13,17 @@ export function getWordPressClient(
     new Notice(plugin.i18n.t('error_noEndpoint'));
     return null;
   }
-  const cached = wpClientsCache.get(plugin.settings.apiType);
-  if (cached) {
-    return cached;
-  } else {
-    let newClient: WordPressClient | null;
-    switch (plugin.settings.apiType) {
-      case ApiType.XML_RPC:
-        newClient = new WpXmlRpcClient(app, plugin);
-        break;
-      case ApiType.RestAPI_miniOrange:
-        newClient = new WpRestClient(app, plugin, new WpRestClientMiniOrangeContext());
-        break;
-      default:
-        newClient = null;
-        break;
-    }
-    if (newClient) {
-      wpClientsCache.set(plugin.settings.apiType, newClient);
-    }
-    return newClient;
+  let client: WordPressClient | null;
+  switch (plugin.settings.apiType) {
+    case ApiType.XML_RPC:
+      client = new WpXmlRpcClient(app, plugin);
+      break;
+    case ApiType.RestAPI_miniOrange:
+      client = new WpRestClient(app, plugin, new WpRestClientMiniOrangeContext());
+      break;
+    default:
+      client = null;
+      break;
   }
+  return client;
 }
