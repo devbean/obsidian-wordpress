15fb5bcafa5b7a77ff9b43a2a18d52817eff699a
Cheng Liang <devbean@outlook.com>
2023-02-22 16:03:35 +0800

    feat: Add options about MathJax output format.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { Plugin } from 'obsidian';
-import { DEFAULT_SETTINGS, WordpressPluginSettings, WordpressSettingTab } from './settings';
+import { DEFAULT_SETTINGS, MathJaxOutputType, WordpressPluginSettings, WordpressSettingTab } from './settings';
 import { addIcons } from './icons';
 import { WordPressPostParams } from './wp-client';
 import { getWordPressClient } from './wp-clients';
 import { I18n } from './i18n';
 import { CommentStatus, PostStatus } from './wp-api';
+import { buildMarked, SafeAny } from './utils';
 import { marked } from 'marked';
 
-// import { mathjax } from 'mathjax-full/js/mathjax';
-// import { TeX } from 'mathjax-full/js/input/tex';
-// import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
-// import { SVG } from 'mathjax-full/js/output/svg';
-// import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
-// import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
+import { mathjax } from 'mathjax-full/js/mathjax';
+import { TeX } from 'mathjax-full/js/input/tex';
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
+import { SVG } from 'mathjax-full/js/output/svg';
+import { LiteAdaptor, liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
+import { MathDocument } from 'mathjax-full/js/core/MathDocument';
 
 export default class WordpressPlugin extends Plugin {
 
@@ -31,64 +33,13 @@ export default class WordpressPlugin extends Plugin {
   private ribbonWpIcon: HTMLElement | null = null;
 
   async onload() {
-    // const adaptor = liteAdaptor();
-    // RegisterHTMLHandler(adaptor);
-    // const mathjax_document = mathjax.document('', {
-    //   InputJax: new TeX({ packages: AllPackages }),
-    //   OutputJax: new SVG({ fontCache: 'local' })
-    // });
-
-    marked.use({
-      extensions: [
-        {
-          name: 'mp_keep_inline',
-          level: 'inline',
-          start: (src) => src.indexOf('$'),
-          tokenizer: (src, tokens) => {
-            const match = src.match(/^\$([^$\n]+?)\$/);
-            if (match) {
-              return {
-                type: 'mp_keep_inline',
-                raw: match[0],
-                text: match[1].trim()
-              };
-            }
-          },
-          renderer: (token) => {
-            return `$${token.text}$`;
-            // const node = mathjax_document.convert(token.text);
-            // return adaptor.innerHTML(node);
-          }
-        },
-        {
-          name: 'mp_keep_block',
-          level: 'block',
-          start: (src) => src.indexOf('\n$$'),
-          tokenizer: (src, tokens) => {
-            const match = src.match(/^\$\$([\s\S]+?)\$\$/);
-            if (match) {
-              return {
-                type: 'mp_keep_block',
-                raw: match[0],
-                text: match[1].trim()
-              };
-            }
-          },
-          renderer: (token) => {
-            return `$$${token.text}$$\n`;
-            // const node = mathjax_document.convert(token.text);
-            // return adaptor.innerHTML(node);
-          }
-        }
-      ]
-    });
-
     console.log('loading obsidian-wordpress plugin');
 
     await this.loadSettings();
-
     // lang should be load early, but after settings
     this.#i18n = new I18n(this.#settings?.lang);
+
+    buildMarked(this.settings);
 
     addIcons();
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+import { liteAdaptor, LiteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
+import { MathDocument } from 'mathjax-full/js/core/MathDocument';
+import { MathJaxOutputType, WordpressPluginSettings } from './settings';
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
+import { mathjax } from 'mathjax-full/js/mathjax';
+import { TeX } from 'mathjax-full/js/input/tex';
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
+import { SVG } from 'mathjax-full/js/output/svg';
+import { marked } from 'marked';
+
 export type SafeAny = any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
 export function openWithBrowser(url: string, queryParams: Record<string, undefined|number|string> = {}): void {
@@ -14,4 +24,70 @@ export function generateQueryString(params: Record<string, undefined|number|stri
 
 export function isPromiseFulfilledResult<T>(obj: SafeAny): obj is PromiseFulfilledResult<T> {
   return !!obj && obj.status === 'fulfilled' && obj.value;
+}
+
+export function buildMarked(settings: WordpressPluginSettings): void {
+  let adaptor: LiteAdaptor | null = null;
+  let mathDocument: MathDocument<SafeAny, SafeAny, SafeAny> | null = null;
+  if (settings.mathJaxOutputType === MathJaxOutputType.SVG) {
+    adaptor = liteAdaptor();
+    RegisterHTMLHandler(adaptor);
+    mathDocument = mathjax.document('', {
+      InputJax: new TeX({ packages: AllPackages }),
+      OutputJax: new SVG({ fontCache: 'local' })
+    });
+  }
+
+  marked.use({
+    extensions: [
+      {
+        name: 'mp_keep_inline',
+        level: 'inline',
+        start: (src) => src.indexOf('$'),
+        tokenizer: (src, tokens) => {
+          const match = src.match(/^\$([^$\n]+?)\$/);
+          if (match) {
+            return {
+              type: 'mp_keep_inline',
+              raw: match[0],
+              text: match[1].trim()
+            };
+          }
+        },
+        renderer: (token) => {
+          if (settings.mathJaxOutputType === MathJaxOutputType.SVG) {
+            if (mathDocument && adaptor) {
+              const node = mathDocument.convert(token.text);
+              return adaptor.innerHTML(node);
+            }
+          }
+          return `$${token.text}$`;
+        }
+      },
+      {
+        name: 'mp_keep_block',
+        level: 'block',
+        start: (src) => src.indexOf('\n$$'),
+        tokenizer: (src, tokens) => {
+          const match = src.match(/^\$\$([\s\S]+?)\$\$/);
+          if (match) {
+            return {
+              type: 'mp_keep_block',
+              raw: match[0],
+              text: match[1].trim()
+            };
+          }
+        },
+        renderer: (token) => {
+          if (settings.mathJaxOutputType === MathJaxOutputType.SVG) {
+            if (mathDocument && adaptor) {
+              const node = mathDocument.convert(token.text);
+              return adaptor.innerHTML(node);
+            }
+          }
+          return `$$${token.text}$$\n`;
+        }
+      }
+    ]
+  });
 }
