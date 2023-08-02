b4c09448f51b0dfb9c4097f7d644035d65ce55f0
Ivan Rybnikov <ivrybn@gmail.com>
2023-08-02 23:04:58 +0300

    Add support for Obsidian comments

--- src/utils.ts
+++ src/utils.ts
@@ -7,34 +7,50 @@ import { WordPressPostParams } from './wp-client';
 import { getWordPressClient } from './wp-clients';
 import WordpressPlugin from './main';
 import { isString } from 'lodash-es';
+import MarkdownItContainer from 'markdown-it-container';
 import { ERROR_NOTICE_TIMEOUT } from './consts';
 
 export type SafeAny = any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-export function openWithBrowser(url: string, queryParams: Record<string, undefined|number|string> = {}): void {
+export function openWithBrowser(
+  url: string,
+  queryParams: Record<string, undefined | number | string> = {}
+): void {
   window.open(`${url}?${generateQueryString(queryParams)}`);
 }
 
-export function generateQueryString(params: Record<string, undefined|number|string>): string {
+export function generateQueryString(
+  params: Record<string, undefined | number | string>
+): string {
   return new URLSearchParams(
     Object.fromEntries(
-      Object.entries(params).filter( ([k, v]) => v!==undefined)
+      Object.entries(params).filter(([k, v]) => v !== undefined)
     ) as Record<string, string>
   ).toString();
 }
 
-export function isPromiseFulfilledResult<T>(obj: SafeAny): obj is PromiseFulfilledResult<T> {
+export function isPromiseFulfilledResult<T>(
+  obj: SafeAny
+): obj is PromiseFulfilledResult<T> {
   return !!obj && obj.status === 'fulfilled' && obj.value;
 }
 
 export function setupMarkdownParser(settings: WordpressPluginSettings): void {
   AppState.getInstance().markdownParser.use(MarkdownItMathJax3Plugin, {
-    outputType: settings.mathJaxOutputType
+    outputType: settings.mathJaxOutputType,
+  });
+
+  AppState.getInstance().markdownParser.use(MarkdownItContainer, 'comment', {
+    marker: '%%',
+    validate: () => true,
+    render: () => '',
   });
 }
 
-
-export function rendererProfile(profile: WpProfile, container: HTMLElement): Setting {
+export function rendererProfile(
+  profile: WpProfile,
+  container: HTMLElement
+): Setting {
   let name = profile.name;
   if (profile.isDefault) {
     name += ' ✔️';
@@ -50,21 +66,27 @@ export function rendererProfile(profile: WpProfile, container: HTMLElement): Set
       desc += ' / 🔒 ******';
     }
   }
-  return new Setting(container)
-    .setName(name)
-    .setDesc(desc);
+  return new Setting(container).setName(name).setDesc(desc);
 }
 
 export function isValidUrl(url: string): boolean {
   try {
     return Boolean(new URL(url));
-  } catch(e) {
+  } catch (e) {
     return false;
   }
 }
 
-export function doClientPublish(plugin: WordpressPlugin, profile: WpProfile, defaultPostParams?: WordPressPostParams): void;
-export function doClientPublish(plugin: WordpressPlugin, profileName: string, defaultPostParams?: WordPressPostParams): void;
+export function doClientPublish(
+  plugin: WordpressPlugin,
+  profile: WpProfile,
+  defaultPostParams?: WordPressPostParams
+): void;
+export function doClientPublish(
+  plugin: WordpressPlugin,
+  profileName: string,
+  defaultPostParams?: WordPressPostParams
+): void;
 export function doClientPublish(
   plugin: WordpressPlugin,
   profileOrName: WpProfile | string,
@@ -72,7 +94,7 @@ export function doClientPublish(
 ): void {
   let profile: WpProfile | undefined;
   if (isString(profileOrName)) {
-    profile = plugin.settings.profiles.find(it => it.name === profileOrName);
+    profile = plugin.settings.profiles.find((it) => it.name === profileOrName);
   } else {
     profile = profileOrName;
   }
@@ -83,7 +105,7 @@ export function doClientPublish(
     }
   } else {
     const noSuchProfileMessage = plugin.i18n.t('error_noSuchProfile', {
-      profileName: String(profileOrName)
+      profileName: String(profileOrName),
     });
     new Notice(noSuchProfileMessage, ERROR_NOTICE_TIMEOUT);
     throw new Error(noSuchProfileMessage);
